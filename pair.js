fcd32f8efdcbed39b05f1aa021f8f0ef4072b688
artexpury925 <artexpury@gmail.com>
2025-11-20 12:37:40 +0300

    Delete pair.js

--- pair.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-    res.send(`<h2>Pairing in progress... Keep this tab open.</h2>`);
-});
-
-export default router;
